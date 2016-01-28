5b7f61df8ac755705a91d50fb86e8d1e97091157
Abel Pacara <abel.pacara@gmail.com>
2016-01-27 23:48:51 -0400

    test 6 hooks

--- _support/orders.sql
+++ _support/orders.sql
@@ -1,28 +1,104 @@
-# SQL Manager 2005 Lite for MySQL 3.7.7.1
-# ---------------------------------------
-# Host     : localhost
-# Port     : 3306
-# Database : orders
+-- MySQL dump 10.16  Distrib 10.1.9-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: orders
+-- ------------------------------------------------------
+-- Server version	10.1.9-MariaDB
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Table structure for table `inventory`
+--
 
-CREATE DATABASE `orders`
-    CHARACTER SET 'latin1'
-    COLLATE 'latin1_swedish_ci';
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventory` (
+  `id_inventory` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `story_id` int(11) DEFAULT NULL,
+  `inventory_quantity` int(11) DEFAULT NULL,
+  `inventory_register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_inventory`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-USE `orders`;
+--
+-- Dumping data for table `inventory`
+--
 
-#
-# Structure for the `products` table : 
-#
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES (3,1,NULL,6,'2016-01-28 02:28:14'),(9,2,NULL,9,'2016-01-28 02:40:49'),(11,1,NULL,4,'2016-01-28 03:41:44'),(12,2,NULL,1,'2016-01-28 03:41:44'),(13,1,NULL,5,'2016-01-28 03:47:15'),(14,2,NULL,5,'2016-01-28 03:47:15');
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
 
 DROP TABLE IF EXISTS `products`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
-  `id_product` text,
+  `id_product` int(11) NOT NULL AUTO_INCREMENT,
   `product_code` text,
   `product_name` text,
-  `product_description` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `product_description` text,
+  PRIMARY KEY (`id_product`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'343','PROCESADOR INTEL 7','CORE I 7 4TA GENERACION'),(2,'44566','TARJETA MADRE','MARCA INTEL');
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stories`
+--
+
+DROP TABLE IF EXISTS `stories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stories` (
+  `id_story` int(11) DEFAULT NULL,
+  `story_name` text,
+  `story_location` text,
+  `story_description` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stories`
+--
+
+LOCK TABLES `stories` WRITE;
+/*!40000 ALTER TABLE `stories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stories` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-01-27 23:48:01
