e9b60d681a3c5553a00d5d0917af7da91cc04017
Abel Pacara <abel.pacara@gmail.com>
2016-01-29 02:05:27 -0400

    ADD_TO_ORDER added

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -9,26 +9,31 @@ class Orders extends CI_Controller
         $this->load->helper('form'); 
         $this->load->model('model_orders');    
     }   
-
+   #############################################################################
 	public function index()
 	{
 		$this->load->view('products/welcome_message');
 	}
-	public function add_product()
+	#############################################################################
+	public function add_to_order()
 	{
-
-		if(isset($_REQUEST["product_code"]))
+		if(isset($_REQUEST["availables_maximum"])>0)
 		{
-			$product_data ['product_code'] = $_REQUEST['product_code'];
-			$product_data ['product_name'] = $_REQUEST['product_name'];
-			$product_data ['product_description'] = $_REQUEST['product_description'];
-
-			$this->model_orders->add_product($product_data);
+			for($i=0; $i<$_REQUEST["availables_maximum"]; $i++)
+			{				
+				if(isset($_REQUEST['inventory_quantity_'.$i]) AND $_REQUEST['inventory_quantity_'.$i]>0)
+				{
+					$inventory_data['product_id'] = $_REQUEST['id_product_'.$i];
+					$inventory_data['inventory_quantity'] = $_REQUEST['inventory_quantity_'.$i];
+					$this->model_orders->add_to_inventory($inventory_data);					
+				}
+			}
 		}
 
-		$view_data['list_products'] = $this->model_orders->get_list_products();
-		$this->load->view('products/add_product', $view_data);
+		$view_data['list_inventory'] = $this->model_orders->get_list_inventory();
+		$this->load->view('products/add_to_order', $view_data);
 	}
+	#############################################################################
 	public function add_to_inventory()
 	{
 		if(isset($_REQUEST["availables_maximum"])>0)
@@ -47,5 +52,20 @@ class Orders extends CI_Controller
 		$view_data['list_inventory'] = $this->model_orders->get_list_inventory();
 		$this->load->view('products/add_to_inventory', $view_data);
 	}
+	#############################################################################
+	public function add_product()
+	{
 
+		if(isset($_REQUEST["product_code"]))
+		{
+			$product_data ['product_code'] = $_REQUEST['product_code'];
+			$product_data ['product_name'] = $_REQUEST['product_name'];
+			$product_data ['product_description'] = $_REQUEST['product_description'];
+
+			$this->model_orders->add_product($product_data);
+		}
+
+		$view_data['list_products'] = $this->model_orders->get_list_products();
+		$this->load->view('products/add_product', $view_data);
+	}
 }
--- application/views/products/add_to_order.php
+++ application/views/products/add_to_order.php
@@ -4,14 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Agregar Producto</title>
+	<title>Agregar Productos a Pedido</title>
 </head>
 <body>
 
 <div id="container">
-	<h1>Agregar Inventario de Productos</h1>	
+	<h1>Agregar Productos a Pedido</h1>	
 	<?php
-	echo form_open_multipart("orders/add_to_order");
+	echo form_open_multipart("orders/add_to_inventory");
 	?>
 	<input type="hidden" name="availables_maximum" value="<?php echo count($list_inventory)?>"/>
 	<table>
@@ -21,6 +21,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<th>Nombre</th>
 			<th>Ultima modificacion</th>
 			<th>Saldo Actual</th>
+			<th>Precio Minimo</th>
+			<th>Precio Maximo</th>
+			<th>Seleccione</th>
 			<th>Cantidad</th>
 		</tr>
 		<?php
@@ -32,8 +35,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<td><?php echo $list_inventory[$i]['product_name']?></td>
 				<td><?php echo $list_inventory[$i]['inventory_last_date']?></td>
 				<td><?php echo $list_inventory[$i]['inventory_quantity']?></td>
+				<td><?php echo $list_inventory[$i]['product_minimum_price']?></td>
+				<td><?php echo $list_inventory[$i]['product_maximum_price']?></td>
+				<td>					
+					<input type="checkbox" name="product_checked_<?php echo $i?>" value="<?php echo $list_inventory[$i]['id_product']?>"/>
+				</td>
 				<td>
-					<input type="hidden" name="id_product_<?php echo $i?>" value="<?php echo $list_inventory[$i]['id_product']?>"/>
 					<input type="text" name="inventory_quantity_<?php echo $i?>"/>
 				</td>
 			</tr>
